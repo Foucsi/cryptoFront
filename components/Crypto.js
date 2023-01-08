d5869c547363e67caece74431a678f3c15d41319
Foucsi <julien.foucartreact@gmail.com>
2023-01-08 19:05:55 +0100

    commit

---
+++ components/Crypto.js
@@ -0,0 +1,43 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function Crypto({ crypto }) {
+  const [price, setPrice] = useState(false);
+
+  useEffect(() => {
+    if (crypto.current_price > crypto.low_24h) {
+      setPrice(true);
+    }
+  }, []);
+
+  return (
+    <View
+      style={{
+        height: 150,
+        width: 150,
+        marginTop: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: price ? "#6DB76B" : "#E5595F",
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          height: "100%",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <Text>{crypto.name}</Text>
+        <Text>$ {crypto.current_price}</Text>
+        <Image
+          style={{ height: 40, width: 40 }}
+          source={{ uri: crypto.image }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
--- screens/WelcomeSreen.js
+++ screens/WelcomeSreen.js
@@ -1,12 +1,49 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useEffect } from "react";
+import Crypto from "../components/Crypto";
 
 export default function WelcomeSreen() {
+  const [arrayCryptos, setArrayCryptos] = useState([]);
   const users = useSelector((state) => state.user.value);
+
+  const listingCrypto = arrayCryptos.map((crypto, index) => {
+    return <Crypto key={index} crypto={crypto} />;
+  });
+
+  useEffect(() => {
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setArrayCryptos(data);
+      });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>WelcomeSreen {users.username} !</Text>
+      <View
+        style={{
+          width: "100%",
+          height: "80%",
+        }}
+      >
+        <ScrollView style={{ flex: 1, padding: 20 }}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              flexWrap: "wrap",
+              alignItems: "center",
+              justifyContent: "space-around",
+            }}
+          >
+            {listingCrypto}
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -17,5 +54,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    flexDirection: "column",
   },
 });
